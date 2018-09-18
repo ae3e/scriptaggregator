8f7a46ac00000f28333fb913d6706461b56b3107
ae3e <dev@ae3e.com>
2018-09-18 11:28:36 +0200

    Plugin extends com.google.inject.AbstractModule to be compatible with Kairosdb 1.3.0

--- src/main/java/com/ae/scriptaggregator/ScriptAggregatorModule.java
+++ src/main/java/com/ae/scriptaggregator/ScriptAggregatorModule.java
@@ -2,19 +2,20 @@ package com.ae.scriptaggregator;
 
 import ch.qos.logback.classic.Logger;
 //import com.google.inject.name.Names;
-import java.util.Properties;
-import org.kairosdb.core.CoreModule;
+//import java.util.Properties;
+import com.google.inject.AbstractModule;
+//import org.kairosdb.core.CoreModule;
 import org.slf4j.LoggerFactory;
 
-public class ScriptAggregatorModule extends CoreModule{
+public class ScriptAggregatorModule extends AbstractModule{
 
 	public static final Logger logger = (Logger) LoggerFactory.getLogger(ScriptAggregatorModule.class);
 
 	
-	public ScriptAggregatorModule(Properties props)
+	/*public ScriptAggregatorModule(Properties props)
 	{
 		super(props);
-	}
+	}*/
 	
 	@Override
 	protected void configure() {
